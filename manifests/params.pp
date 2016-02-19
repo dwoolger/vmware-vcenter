9a69934b043e3a53706f583642d4e85e25e5ed29
Gerrard Geldenhuis <gerhardus.geldenhuis@gmail.com>
2016-02-19 10:06:09 +0000

    Making params PE only for now

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,12 +2,20 @@
 # vCenter common parameters
 class vcenter::params {
 
+# Because PuppetLabs broke the puppetversion fact we need to fudge this temporarily 
+# to get it moving forward. Once there is a reliable way to determine the Puppet Version 
+# and determine whether it is PE then we can kill this fudge. Until then...
+
   if $::puppetversion =~ /Puppet Enterprise/ {
-    $provider  = 'pe_gem'
-    $ruby_path = '/opt/puppet/bin/ruby'
+    $provider  = 'puppet_gem'
+    $ruby_path = '/opt/puppetlabs/puppet/bin/gem'
   } else {
-    $provider  = 'gem'
-    $ruby_path = '/usr/bin/env ruby'
+
+# The commented out values should be put back once we can determine reliably if we are running PE.
+#    $provider  = 'gem'
+#    $ruby_path = '/usr/bin/env ruby'
+    $provider  = 'puppet_gem'
+    $ruby_path = '/opt/puppetlabs/puppet/bin/gem'
   }
 
 }
